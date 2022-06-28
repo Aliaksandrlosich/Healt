a1598cf59d0afcc4a66e6cd8d9f3a1a617cd87f2
alexandrLosich <alexandr.losich@iptel.co>
2022-06-29 00:14:44 +0300

    Add sort in medication list

--- src/repository/Medication.js
+++ src/repository/Medication.js
@@ -6,15 +6,18 @@ class MedicationRepository extends Repository {
  }
 
  async addMedicationToList ({ name, description, initCount, destinationCount, userId }) {
-  const newMedication = await this.query(`INSERT INTO medication (name, description, initial_count, destination_count, user_id) 
-    VALUES ('${name}', '${description}', ${initCount}, ${destinationCount}, '${userId}') RETURNING id;`)
+  const newMedication = await this.query(`INSERT INTO medication (name, description, initial_count, destination_count, 
+                    user_id, updated_date) 
+    VALUES ('${name}', '${description}', ${initCount}, ${destinationCount}, '${userId}', NOW()) RETURNING id;`)
 
   return newMedication.rows[0]
  }
 
  async getMedicationsList ({ userId }) {
   const medicationsList = await this.query(`SELECT name, description, initial_count as initCount, 
-        destination_count as destinationCount, id FROM medication WHERE user_id='${userId}';`)
+        destination_count as destinationCount, id FROM medication 
+        WHERE user_id='${userId}'
+        ORDER BY updated_date DESC;;`)
 
   return medicationsList.rows
 
@@ -22,7 +25,8 @@ class MedicationRepository extends Repository {
 
  async updateMedication ({ medicationId, name, description, initCount, destinationCount }) {
   const medication = await this.query(`UPDATE medication
-            SET name = '${name}', description = '${description}', initial_count = ${initCount}, destination_count = ${destinationCount}
+            SET name = '${name}', description = '${description}', initial_count = ${initCount},
+             destination_count = ${destinationCount}, updated_date = NOW()
             WHERE id = ${medicationId} RETURNING id;`)
 
   return medication.rows[0]
